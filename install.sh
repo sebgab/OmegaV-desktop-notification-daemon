cdac5ad080b06a662979984536e49646b3eb97e0
Sebastian H. Gabrielli <sebgab@sebaweb.no>
2023-05-16 19:18:26 +0200

    Reamped install script
    The install script is now more handholdy.
    
    Upon running the installer it will check if the system is debian based.
    If so it will install the required dependencies, if not it will inform the user of the dependency.
    
    The installer will check if the compile and install of the rust program was successful.
    
    The installer will only add the icon, if the icon is not already added to the cdg list.
    
    Upon installer exit it will inform the user about how many errors occured during installation.

--- install.sh
+++ install.sh
@@ -1,8 +1,45 @@
-echo "Compiling and installing program"
-cargo install --path .
+# Create a variable to see if the script ended with an error
+SCRIPT_ERR=0
 
-echo "Adding the notification icon to the xdg list"
-xdg-icon-resource install --novendor OV.png --size 200
+# Check if the user is running a debian derivative
+if ls /usr/bin/apt >/dev/null 2>&1; then
+    echo "Debian based system detected, automatically installing dependencies"
 
-echo "Installation is completed."
+    # Check if they have the depenency installed
+    if dpkg -s "libssl-dev" >/dev/null 2>&1; then
+        echo "All dependecies are installed!"
+    else
+        # If not, install it
+        echo "dependency \"libssl-dev\" is not installed."
+        echo "installing..."
+        sudo apt install libssl-dev -y
+    fi
+else
+    # Non debian based distros have to manually install
+    echo "-------------------------------------------------------------------------------------------------------"
+    echo "You may need to install an ssl development package to compile this program."
+    echo "On debian and ubuntu it is called \`libssl-dev\`. Please install the equivalent for your distribution."
+    echo "-------------------------------------------------------------------------------------------------------"
+fi
+
+# Install the program
+if ! cargo install --path .; then
+    # If the install failed warn the user
+    echo "\033[0;31Failed to install programm.\033[0m]"
+    echo "Are you missing the libssl-dev dependency?"
+    SCRIPT_ERR+=1
+fi
+
+# Check if OV icon already exists
+if ls ~/.local/share/icons/hicolor/200x200/apps/OV.png >/dev/null 2>&1; then
+    echo "OV icon is already installed."
+else
+    echo "Adding the OV icon to the xdg list"
+    if ! xdg-icon-resource install --novendor OV.png --size 200; then
+        echo "Failed to add OV icon"
+        SCRIPT_ERR+=1
+    fi
+fi
+
+echo "Install completed with $SCRIPT_ERR errors."
 echo "Enjoy!"
