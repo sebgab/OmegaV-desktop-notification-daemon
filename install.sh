f660f3a38f08d86da964673c08d467363e46f43b
Sebastian H. Gabrielli <sebgab@sebamail.no>
2023-05-23 23:23:33 +0200

    Added more comments & Added colour

--- install.sh
+++ install.sh
@@ -5,13 +5,17 @@ SCRIPT_ERR=0
 
 #TODO: Check if rust is installed, and handle that case.
 
+#
+# Dependency installation
+#
+
 # Check if the user is running a debian derivative
 if ls /usr/bin/apt >/dev/null 2>&1; then
     echo "Debian based system detected, automatically installing dependencies."
 
     # Check if they have the depenency installed
     if dpkg -s "libssl-dev" >/dev/null 2>&1; then
-        echo "All dependecies are installed!"
+        echo -e "\e[32mAll dependecies are installed!\e[0m"
     else
         # If not, install it
         echo "dependency \"libssl-dev\" is not installed."
@@ -20,11 +24,15 @@ if ls /usr/bin/apt >/dev/null 2>&1; then
     fi
 else
     # Non debian based distros have to manually install
-    echo "-------------------------------------------------------------------------------------------------------"
-    echo "You may need to install an ssl development package to compile this program."
-    echo "On debian and ubuntu it is called \`libssl-dev\`. Please install the equivalent for your distribution."
-    echo "-------------------------------------------------------------------------------------------------------"
+    echo -e "\e[1;33m-------------------------------------------------------------------------------------------------------\e[0m"
+    echo -e "\e[1;33mYou may need to install an ssl development package to compile this program.\e[0m"
+    echo -e "\e[1;33mOn Debian and Ubuntu it is called \`libssl-dev\`. Please install the equivalent for your distribution.\e[0m"
+    echo -e "\e[1;33m-------------------------------------------------------------------------------------------------------\e[0m"
 fi
+
+#
+# Program installation
+#
 
 # Install the program
 if ! cargo install --path .; then
@@ -34,17 +42,27 @@ if ! cargo install --path .; then
     SCRIPT_ERR+=1
 fi
 
+#
+# Icon installtion
+#
+
 # Check if OV icon already exists
 if ls ~/.local/share/icons/hicolor/200x200/apps/OV.png >/dev/null 2>&1; then
-    echo "OV icon is already installed."
+    echo -e "\e[32mOV icon is already installed.\e[0m"
 else
     echo "Adding the OV icon to the xdg list"
     if ! xdg-icon-resource install --novendor OV.png --size 200; then
-        echo "\033[0;31Failed to add OV icon\033[0m"
+        echo -e "\e[31mFailed to add OV icon\e[0m"
         SCRIPT_ERR+=1
     fi
 fi
 
+#
+# Desktop file installation
+#
+
+# Take backup of default desktop file
+cp omegav-daemon.desktop omegav-daemon.desktop.default
 # Replace the username with the proper user
 username=$(whoami)
 sed -i "s/\$USER/$username/g" omegav-daemon.desktop
@@ -54,5 +72,6 @@ xdg-desktop-menu install omegav-daemon.desktop
 # Add it to autostart
 ln -s ~/.local/share/applications/omegav-daemon.desktop ~/.config/autostart
 
+# Inform the user of the result of the installation.
 echo "Install completed with $SCRIPT_ERR errors."
 echo "Enjoy!"
